81d52422be81634fa2bed9e5f482c0da13f75077
unknown <malufreirejau@yahoo.com.br>
2022-05-03 16:48:15 -0300

    adding first jump

--- script.js
+++ script.js
@@ -2,8 +2,32 @@ const dino = document.querySelector('.dino');
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        console.log('espaço');
+        jump();
     }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval((downInterval));
+                }
+                else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }
+        else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
