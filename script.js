deca1188eee7ef8b87896e8d99d2c62e007b8986
unknown <malufreirejau@yahoo.com.br>
2022-05-03 17:22:04 -0300

    detecting collision

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -11,8 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
-
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -50,6 +49,9 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h2 class="game-over">Fim de jogo :(</h2>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
